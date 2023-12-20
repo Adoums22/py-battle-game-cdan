7c4296fa14134928eb79af68069f6870b924e20b
Adel <adel.saadna22@gmail.com>
2023-12-20 13:53:19 +0100

    Implementation de la class avec geteur et seteur

---
+++ battle-game/gears/armor.py
@@ -0,0 +1,18 @@
+
+
+class Armor:
+    def __init__(self, name:str, defense:int = 100) :
+        self.name = name
+        self.defense = defense
+        
+    def get_name(self) : 
+        return self.name
+    
+    def get_defense(self) :
+        return self.defense
+    
+    def set_name(self, new_name) :
+        self.name = new_name
+    
+    def set_defense(self, new_defense) :
+        self.defense = new_defense
